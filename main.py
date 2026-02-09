1a12f7d0722e25eac7cb0fb843306fe31c3e57c3
Aayushi <aayushi.purohit.btech2028@atlasskilltech.university>
2026-02-09 09:14:44 +0530

    Updated main to call add sub mul

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from add import add
+from sub import sub
+from mul import mul
+
 def main():
-    print("Calculator App")
+    a, b = 10, 5
+    print("Add:", add(a, b))
+    print("Sub:", sub(a, b))
+    print("Mul:", mul(a, b))
 
 if __name__ == "__main__":
     main()
